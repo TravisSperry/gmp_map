7a90fd708bede436d737b8f8ac4d0bd52480dc3a
tkendalls <director@mathplusacademy.com>
2016-04-11 16:27:16 -0400

    update

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,5 +1,6 @@
 class SubscribersController < ApplicationController
   # before_action :set_subscriber, only: [:show, :edit, :update, :destroy]
+  skip_before_filter :verify_authenticity_token, :only => [:new_subscriber]
 
   def new_subscriber
     if params.has_key?(:data)
